74f3fd7b3ce94dd002f5d86a441b00dadf1f0195
nyuta <nyuta.main@gmail.com>
2023-04-02 18:48:44 +0900

    🎉 begin pet-match

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, MantineProvider, Text } from "@mantine/core";
+
+function App() {
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Text>Welcome to Mantine!</Text>
+      <Button>Click Me</Button>
+    </MantineProvider>
+  );
+}
+
+export default App;
