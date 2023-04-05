5960da6be8dd80ce3704d33c8c6396aeebf4ded7
nyuta <nyuta.main@gmail.com>
2023-04-06 08:53:42 +0900

    ✨ introduce storybook

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ["plugin:react/recommended", "plugin:react-hooks/recommended", "prettier"],
+  extends: ["plugin:react/recommended", "plugin:react-hooks/recommended", "prettier", "plugin:storybook/recommended"],
   overrides: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module",
+    sourceType: "module"
   },
   plugins: ["react", "@typescript-eslint", "jsx-a11y"],
   rules: {},
   settings: {
     react: {
-      version: "detect",
-    },
-  },
+      version: "detect"
+    }
+  }
 };
